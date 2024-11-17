d9f897e20c46e7ffa7c3c904d3bea40344b613f0
nop <llkl8526@gmail.com>
2024-11-17 16:53:11 +0100

    fix: env variable of backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 import AppService from '../services/AppService';
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000"
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 });
 
-axiosInstance.interceptors.request.use(async config => {
-	const accessToken = AppService.getToken();
-	if (accessToken) {
-		config.headers.Authorization = `Bearer ${accessToken}`;
-	}
-	return config;
+axiosInstance.interceptors.request.use(async (config) => {
+  const accessToken = AppService.getToken();
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
 });
 
 axiosInstance.interceptors.response.use(
-	async (res) => res?.data,
-	(err) => {
-		if (err.response?.status === 401) {
-			AppService.clearToken();
-		}
-		return Promise.reject(err);
-	}
+  async (res) => res?.data,
+  (err) => {
+    if (err.response?.status === 401) {
+      AppService.clearToken();
+    }
+    return Promise.reject(err);
+  }
 );
 
 export default axiosInstance;
